48c92085710ee8c6e8dafc1f0e293988a052233e
lephuthuc2001 <lephuthuc2001@gmail.com>
2023-03-04 11:59:55 +0700

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
 
+// const Car = require("./models/Car");
+
+require("dotenv/config");
+// const csv = require("csvtojson");
+// const csvFilePath = path.join(__dirname, "data.csv");
+// csv()
+//   .fromFile(csvFilePath)
+//   .then((jsonObj) => {
+//     let cars = structuredClone(jsonObj);
+//     for (car of cars) {
+//       car.release_date = parseInt(car.release_date);
+//       car.price = parseInt(car.price);
+//       car.isDeleted = false;
+//     }
+//     Car.insertMany(cars)
+//       .then(() => {
+//         console.log("Insert Success");
+//       })
+//       .then((err) => {
+//         console.log("Insert Error", err);
+//       });
+//     /**
+//      * [
+//      * 	{a:"1", b:"2", c:"3"},
+//      * 	{a:"4", b:"5". c:"6"}
+//      * ]
+//      */
+//   });
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MONGODB
 mongoose.connect(process.env.MONGO_URI, () => {
-	console.log('Connected to Database!');
+  console.log("Connected to Database!");
 });
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,97 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    await Car.create({ ...req.body, isDeleted: false });
+    res.send({
+      message: "Create Car Successfully!",
+      car: {
+        ...req.body,
+      },
+    });
+  } catch (error) {
+    res.send({
+      message: "Create Car Failed!",
+    });
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    let { page, search } = req.query;
+    search = search;
+    page = parseInt(page);
+
+    let query;
+
+    if (search) {
+      query = { isDeleted: false, $text: { $search: search } };
+    } else {
+      query = { isDeleted: false };
+    }
+
+    let cars = await Car.find(query)
+      .sort({ createdAt: "desc", updatedAt: "desc" })
+      .select({
+        isDeleted: 0,
+        createdAt: 0,
+        updatedAt: 0,
+        __v: 0,
+      });
+
+    const total = Math.ceil(cars.length / 10);
+
+    cars = cars.slice((page - 1) * 10, (page + 1) * 10);
+
+    res.send({ cars, total });
+  } catch (err) {
+    res.send(err.message);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const { id } = req.params;
+    const editedCar = await Car.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      {
+        new: true,
+        select: {
+          isDeleted: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+        },
+      }
+    );
+    res.send({
+      message: "Update Car Successfully!",
+      car: editedCar,
+    });
+  } catch (err) {
+    res.send({
+      message: "Update Car Fail!",
+    });
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const { id } = req.params;
+
+    const deletedCar = await Car.findByIdAndUpdate(id, { isDeleted: true });
+    res.send({
+      message: "Delete Car Successfully!",
+      car: deletedCar,
+    });
+  } catch (err) {
+    res.send({
+      message: "Delete Car Failed!",
+    });
+  }
 };
 
 module.exports = carController;
